ec171798380f247c15f74146986f649a291db3d3
Ikigu <ikigu.geo@gmail.com>
2024-05-26 21:11:48 +0300

    feat: add a stats api endpoint
    This returns a count of all the object classes in the database

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,37 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Returns count of each object class in the database"""
+
+    objects = {
+        "amenities": Amenity,
+        "cities": City,
+        "place": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+
+    objects_dict: dict = {}
+
+    for key, value in objects.items():
+        objects_dict[key] = storage.count(value)
+
+    return jsonify(objects_dict)
