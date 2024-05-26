c9a5f7af7f8d0d6e2341ee9e554d09b0e08d3ce1
Mabongakev <kelvin.mabonga92@gmail.com>
2024-05-26 14:06:22 -0700

    error handlling

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,10 +15,14 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
     """Tears down the storage"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """return 404"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
