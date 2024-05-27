bc86d1adc69e7d12bebfdcb613113ab8ecd20f0a
Ikigu <ikigu.geo@gmail.com>
2024-05-27 23:53:45 +0300

    feat: allow CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,15 @@ Creates the app.
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
