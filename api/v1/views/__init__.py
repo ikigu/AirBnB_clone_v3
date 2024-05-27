b9c946ba9ff90c67ea7e71fd780bf89d8956a3ce
Ikigu <ikigu.geo@gmail.com>
2024-05-27 14:07:05 +0300

    fix: import modules in correct areas to avoid cyclic imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,11 @@
 
 """Initiates the views module"""
 
-from api.v1.views.cities import *
-from api.v1.views.states import *
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint("api_v1", __name__, url_prefix='/api/v1')
+
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.index import *
+
