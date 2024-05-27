f02fde32197ce6c45fa87883fff89433cbc6d9a5
Ikigu <ikigu.geo@gmail.com>
2024-05-27 16:25:46 +0300

    fix: import jsonify

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ Creates the app.
 """
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -19,10 +19,12 @@ def teardown(exception):
     """Tears down the storage"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """return 404"""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
