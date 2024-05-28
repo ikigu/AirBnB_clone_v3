2932bb640685905fc6eb1356519054726fbcff6e
Ikigu <ikigu.geo@gmail.com>
2024-05-29 02:24:36 +0300

    feat: add ability to search for state or city based on amenities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,7 @@ from flask import abort, jsonify, make_response, request
 from models import storage
 from models.city import City
 from models.place import Place
+from models.state import State
 from models.user import User
 
 
@@ -112,3 +113,68 @@ def update_place(place_id):
     storage.save()
 
     return jsonify(place_to_update.to_dict())
+
+
+@app_views.route("/places_search", methods=["POST"], strict_slashes=False)
+def places_search():
+    """Searches for places"""
+
+    search_kwargs = request.get_json(silent=True)
+
+    if not search_kwargs:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    json_empty = True
+
+    if search_kwargs != {}:
+        json_empty = False
+
+    for value in search_kwargs.values():
+        if len(value) > 0:
+            json_empty = False
+            break
+
+    places = []
+
+    if json_empty == True:
+        places = [place for place in storage.all(Place).values()]
+
+    states_list_in_search_kwargs = search_kwargs.get("states", [])
+
+    if len(states_list_in_search_kwargs) > 0:
+
+        for state_id in states_list_in_search_kwargs:
+            state = storage.get(State, state_id)
+
+            for city in state.cities:
+                places = places + [place for place in city.places]
+
+    cities_list_in_search_kwargs = search_kwargs.get("cities", [])
+
+    if len(cities_list_in_search_kwargs) > 0:
+
+        for city_id in cities_list_in_search_kwargs:
+            city = storage.get(City, city_id)
+
+            if not city:
+                abort(404)
+
+            places = places + [place for place in city.places]
+
+    unique_places = []
+
+    for place in places:
+        if place not in unique_places:
+            unique_places.append(place)
+
+    amenities_list_in_search_kwargs = search_kwargs.get("amenities", [])
+
+    if len(amenities_list_in_search_kwargs) > 0:
+        for place in unique_places:
+            for amenity in amenities_list_in_search_kwargs:
+                if amenity not in place.amenities:
+                    unique_places.remove(place)
+
+    places_dicts = [place.to_dict() for place in unique_places]
+
+    return jsonify(places_dicts)
